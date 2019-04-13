21534857561fdfd07f8031105acf9a974aa36de2
playertr <tplayer@hmc.edu>
2019-04-13 11:24:51 -0700

    initial push of FSM

---
+++ target_land_test_FSM/nav_helper_funcs.py
@@ -0,0 +1,213 @@
+# nav_helper_funcs.py
+# Tim Player
+# 3 April 2019
+
+from collections import deque
+from imutils.video import VideoStream
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video import FPS
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import imutils
+import time
+import cv2  # version 2.4.13.6 https://docs.opencv.org/2.4.13.6/
+import numpy as np
+import datetime
+from dronekit import VehicleMode, connect
+from pymavlink import mavutil
+
+
+def make_headers(file_name):
+
+    f = open(file_name, 'a+')
+    f.write('{0} {1:^1} {2:^1} {3:^1} {4:^1} {5:^1} {6:^1}\n'.format(
+        'Lat', 'Lon', 'Alt', 'X-plat', 'Y-plat', 'X-px', "Y-px"))
+    f.close()
+
+
+def send_land_message(x, y, dist):
+    """Sends MAV: LANDING_TARGET_ENCODE message to copter
+
+    Arguments:
+        x {int} -- target x centroid in pixels
+        y {int} -- target y centroid in pixels
+        dist {[type]} -- [description]
+    """
+    # radians
+    x_rad = (x-horizontal_resolution/2)*horizontal_fov/horizontal_resolution
+    y_rad = (y-vertical_resolution/2)*vertical_fov/vertical_resolution
+
+    msg = vehicle.message_factory.landing_target_encode(
+        0,       # time_boot_ms (not used)
+        0,       # target num
+        mavutil.mavlink.MAV_FRAME_BODY_NED,  # frame
+        x_rad,  # x-axis angular offset
+        y_rad,  # y-axis angular offset
+        dist,      # distance to target, in meters
+        0, 0)     # size of target in radians
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+
+def move_pos(vehicle, x, y, z):
+    """
+    Move, in the body frame, x meters forward, y meters right, and z 
+    meters down
+
+    Arguments:
+        vehicle {Vehicle} -- dronekit Vehicle object
+        x {float} -- meters to move forward
+        y {float} --  meters to move right
+        z {float} -- meters to move down
+    """
+
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,
+        0,
+        0,
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+        0b110111111000,  # Position typemask
+        x,  # dx forward (meters)
+        y,  # dy right (meters)
+        z)  # dz down (meters)
+
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+
+def move_vel(vehicle, vx, vy, vz):
+    """
+    Move, in the body-oriented frame, x m/s forward, y m/s right, and z 
+    m/s down
+
+    Arguments:
+        vehicle {Vehicle} -- dronekit Vehicle object
+        vx {float} -- m/s forward
+        vy {float} -- m/s right
+        vz {float} -- m/s down
+    """
+
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,
+        0,
+        0,
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+        0b110111000111,  # Velocity typemask
+        x,  # dx forward (meters)
+        y,  # dy right (meters)
+        z)  # dz down (meters)
+
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+
+def terminate_flight(vehicle):
+    """
+    Sends a MAV_CMD_DO_FLIGHTTERMINATION message. Immediately kills motors,
+    causing the copter to a) fall from the air, or b) cease to macerate your
+    fingers.
+
+    May currently have the wrong number of parameters.
+
+    https://mavlink.io/en/messages/common.html#MAV_CMD_DO_FLIGHTTERMINATION
+
+    Arguments:
+        vehicle {Vehicle} -- dronekit Vehicle object
+    """
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target_system, target_component
+        mavutil.mavlink.MAV_CMD_DO_FLIGHTTERMINATION,  # command
+        1,  # confirmation
+        0,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        0,          # param 3, direction -1 ccw, 1 cw
+        0,  # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+
+def log_data(file_name, vehicle, x_m, y_m, x_pix, y_pix):
+
+    f = open(file_name, 'a+')
+
+    # edit this line to have data logging of the data you care about
+    data = [str(x) for x in [vehicle.location.global_relative_frame.lat,
+                             vehicle.location.global_relative_frame.lon,
+                             vehicle.location.global_relative_frame.alt,
+                             x_m, y_m, x_pix, y_pix]]
+
+    f.write(' '.join(data) + '\n')
+    f.close()
+
+
+def find_target(vs):
+
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(
+        frame, width=horizontal_resolution, height=vertical_resolution)
+
+    blurred = cv2.GaussianBlur(frame, (3, 3), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "purple", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, colorLower, colorHigher)
+    mask = cv2.erode(mask, None, iterations=1)
+    mask = cv2.dilate(mask, None, iterations=1)
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the target
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0]  # if imutils.is_cv2() else cnts[1]
+    center = None
+
+    # initialize return values to None
+    (x_m, y_m, x_pix, y_pix) = (None, None, None, None)
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+
+        if M["m00"] != 0:
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        else:
+            center = (0, 0)
+
+        # only proceed if the radius meets a minimum size
+        if radius > min_radius:
+            # print("Landing")
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            # cv2.circle(frame, (int(x), int(y)), int(radius),
+            #    (255, 255, 0), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+            z = vehicle.location.global_relative_frame.alt
+            # print("Alt: ", z)
+            m_per_pix_x = (2*z*math.tan(horizontal_fov/2)) / \
+                horizontal_resolution
+            m_per_pix_y = (2*z*math.tan(vertical_fov/2)) / \
+                vertical_resolution
+
+            x_m = (x-horizontal_resolution/2)*m_per_pix_x
+            y_m = -(y-vertical_resolution/2)*m_per_pix_y
+            dist = math.sqrt(x_m*x_m + y_m*y_m + z*z)
+            x_pix = (x-horizontal_resolution/2) * \
+                horizontal_fov/horizontal_resolution
+            y_pix = (y-vertical_resolution/2) * \
+                vertical_fov/vertical_resolution
+
+    return (x_m, y_m, x_pix, y_pix)
---
+++ target_land_test_FSM/target_land_test_v2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+
+# target_land_test_v2.py
+# Tim Player and Andrew Pham
+# Questions? tplayer@hmc.edu, apham@hmc.edu
+# Development version 29 March 2019
+
+import global_params
+from Landing_States import Initial_Descent_State, \
+    Final_Descent_State, Restart_State, Landed_State
+
+#####################################################################
+# main method
+#####################################################################
+
+
+def main():
+    # start input video stream
+    vs = PiVideoStream(resolution=(
+        og_horz_resolution, og_vert_resolution)).start()
+
+    # label this capture
+    start_time = datetime.datetime.now().replace(microsecond=0).strftime(
+        '%y-%m-%d %H.%M.%S')
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.cv.CV_FOURCC(*'DIVX')
+    video_file = "Captures/camera_cap_" + \
+        start_time + ".avi"
+    out = cv2.VideoWriter(video_file, fourcc, fps.fps(),
+                          (horizontal_resolution, vertical_resolution))
+
+    vehicle = connect('/dev/serial0', wait_ready=False, baud=57600)
+
+    # Make Log headers
+    log_name = 'Log/Bot_' + start_time + '.txt'
+    make_headers(log_name)
+
+    # control loop
+    state = Searching_State()
+
+    while(repr(state) != "Landed_State"):
+        try:
+            state.executeControl(vs, vehicle, out)
+            state = state.transition()
+
+        except (KeyboardInterrupt, SystemExit):
+            break
+
+    terminate_flight(vehicle)
+
+
+if __name__ == "__main__":
+    main()
